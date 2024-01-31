cfb1708c8658dbf0bd10b9ad919cd5272ae051d9
Tom Ribaroff <tomribaroff@gmail.com>
2024-01-31 09:26:47 -0500

    fix upload error to bucket

--- Pipeline-Steps/save_item_to_bucket.py
+++ Pipeline-Steps/save_item_to_bucket.py
@@ -1,10 +1,11 @@
-from google.cloud import storage
+from gcloud import storage
 from oauth2client.service_account import ServiceAccountCredentials
 import os
 import json
 import pandas as pd
+from pathlib import Path 
 
-def save_item_to_bucket(input_file_path, input_file, bucket):
+def save_item_to_bucket(input_file_path, bucket_file_path, bucket_name):
 
     with open('Pipeline-Steps/high-producer-412815-3b916fb32033.json') as json_file:
         data = json.load(json_file)
@@ -13,32 +14,18 @@ def save_item_to_bucket(input_file_path, input_file, bucket):
 
     client = storage.Client(credentials=credentials, project='high-producer-412815')
 
-    bucket = client.get_bucket(bucket)
-    blob = bucket.blob(input_file_path)
-    blob.upload_from_string(name) 
-
-input_file_path = 'Saved_DataFrames/2024-01-30/2024-01-30.csv'
-input_file = pd.read_csv('Saved_DataFrames/2024-01-30/2024-01-30.csv')
-bucket = batch_prediction_store_bucket
-save_item_to_bucket(input_file_path, input_file,'batch_prediction_store_bucket')
-
+    bucket = client.get_bucket(bucket_name)
+    #blob = bucket.blob(input_file_path)
+    #blob.upload_from_string(name) 
+       
     # Create a blob in the bucket
-    blob = bucket.blob(destination_blob_name)
+    blob = bucket.blob(bucket_file_path)
 
     # Upload the contents of the CSV file to the blob
     with open(input_file_path, 'rb') as csv_file:
-        blob.upload_from_file(input_file, content_type='application/octet-stream')
+        blob.upload_from_filename(input_file_path) #the opposite function is download_to_filename
 
-
-def save_item_from_bucket(file_name, bucket):
-
-    with open('Pipeline-Steps/high-producer-412815-3b916fb32033.json') as json_file:
-        data = json.load(json_file)
-    
-    credentials = ServiceAccountCredentials.from_json_keyfile_dict(data)
-
-    client = storage.Client(credentials=credentials, project='high-producer-412815')
-
-    bucket = client.get_bucket(bucket)
-    blob = bucket.blob(file_name)
-    blob.download_as_text() 
+input_file_path = Path('./Pipeline-Steps/Saved_DataFrames/2024-01-31/2024-01-31.csv')
+bucket_file_path = '2024-01-31/2024-01-31.csv'
+bucket = 'batch_prediction_store_bucket'
+save_item_to_bucket(input_file_path, bucket_file_path,'batch_prediction_store_bucket')
