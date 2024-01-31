673bf6e959eced9d319c1f0aada9106cd7973f6e
Tom Ribaroff <tomribaroff@gmail.com>
2024-01-30 22:00:54 -0500

    fix storage auth problem

--- Pipeline-Steps/save_item_to_bucket.py
+++ Pipeline-Steps/save_item_to_bucket.py
@@ -2,20 +2,27 @@ from gcloud import storage
 from oauth2client.service_account import ServiceAccountCredentials
 import os
 import json
+from importlib.metadata import version
 
 def save_item_to_bucket(input_file_path, bucket, name):
 
-    with open('high-producer-412815-3b916fb32033.json') as json_file:
+    with open('Pipeline-Steps/high-producer-412815-3b916fb32033.json') as json_file:
         data = json.load(json_file)
     
     credentials = ServiceAccountCredentials.from_json_keyfile_dict(data)
 
     client = storage.Client(credentials=credentials, project='high-producer-412815')
 
-    print(client, credentials)
     bucket = client.get_bucket(bucket)
-    blob = bucket.blob(input_file_path)
-    blob.upload_from_filename(name) 
+    print(version('construct'))
+    blobs = client.list_blobs(batch_prediction_store_bucket)
+    #blob = bucket.blob(input_file_path)
+    #blob.upload_from_string(name) 
+    # Note: The call returns a response only when the iterator is consumed.
+    for blob in blobs:
+        print(blob.name)
 
 save_item_to_bucket('Dockerfile', 'batch_prediction_store_bucket','myDockerPracticeFile')
+
+
 
