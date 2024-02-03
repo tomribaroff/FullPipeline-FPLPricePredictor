c46c1e64404314f63b683a85a769b800bc6a4960
Tom Ribaroff <tomribaroff@gmail.com>
2024-02-03 11:17:56 -0500

    write save_item_from_bucket.py

--- save_item_from_bucket.py
+++ save_item_from_bucket.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 import date
 
 
-def save_file_to_bucket(input_file_path_csv, input_file_path_json, bucket_file_path, bucket_name):
+def save_file_from_bucket(local_file_path, bucket_file_path, bucket_name):
 
     load_dotenv()
     creds = os.getenv('JSON_SA_BATCH_PREDICTION_PATH')
@@ -23,24 +23,16 @@ def save_file_to_bucket(input_file_path_csv, input_file_path_json, bucket_file_p
 
     bucket = client.get_bucket(bucket_name)
 
-    # Create a blob in the bucket
+    # Get the blob from the bucket
     blob = bucket.blob(bucket_file_path)
 
-    # Upload the contents of the CSV file to the blob
-    with open(input_file_path_csv, 'rb') as csv_file:
-        blob.upload_from_filename(input_file_path_csv) #the opposite function is download_to_filename
-
-    # Upload the contents of the json file to the blob
-    with open(input_file_path_json, 'rb') as json_file:
-        blob.upload_from_filename(input_file_path_json) #the opposite function is download_to_filename
+    # Download the blob contents to the local file
+    blob.download_to_filename(local_file_path)
 
 # Get today's date
 today = date.today() 
 
 input_file_path_csv = Path('./Pipeline-Steps/Saved_Data/{}/{}.csv'.format(today,today)) 
-input_file_path_json = Path('./Pipeline-Steps/Saved_Data/{}/{}.json'.format(today,today)') 
-#bucket_file_path = '2024-01-31/2024-01-31.csv' #TODO make sure these dates change dynamically 
 bucket_file_path = today
 bucket = 'batch_prediction_store_bucket'
-save_file_to_bucket(input_file_path_csv, input_file_path_json, bucket_file_path, bucket)
-
+save_file_from_bucket(input_file_path, bucket_file_path, bucket)
