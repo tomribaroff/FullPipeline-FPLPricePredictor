4912640f57364c135a1ced445181e2b88b006995
Tom Ribaroff <tomribaroff@gmail.com>
2024-02-03 16:08:57 -0500

    Change folder name, and incorporate feature_engineering into create_todays_data

--- pipeline/create_todays_dataframe.py
+++ pipeline/create_todays_dataframe.py
@@ -1,6 +1,15 @@
-def create_todays_dataframe():
+from pipeline import feature_engineering
+
+def create_todays_dataframe(overall_events_data: pd.DataFrame -> pd.DataFrame):
     # Create DataFrame from dictionary of all this transfer data
     # Player transfer data from the API is updated at least every 40 minutes, and probably every 15 - 30 mins. 
+
+        all_players_price_change_for_current_event = feature_engineering.extract_all_players_price_changes_this_event(todays_player_data)
+        total_active_players_estimate = feature_engineering.estimate_active_players(overall_events_data)
+        boolean_list_of_players_injured = feature_engineering.extract_all_players_injured(todays_player_data)
+        all_players_prices_today = feature_engineering.extract_all_players_prices_now(todays_player_data)
+        net_transfers_overall_today = feature_engineering.calculate_net_transfers_overall(todays_player_data)
+
 
     price_change_dict = {
         "price_change_this_night": numpy.nan, #currently unknown
